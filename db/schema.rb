72fed20ad997fe920957452c02f8e3bf571b4acc
edsant1 <edwins10329@gmail.com>
2014-09-04 16:09:50 -0600

    Added authentication  -installed Gem for bcryptor  -created test user  -found test user in db  -modified user path to user_url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903200018) do
+ActiveRecord::Schema.define(version: 20140904212940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20140903200018) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
